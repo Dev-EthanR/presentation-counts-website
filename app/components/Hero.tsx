ccf1fe5e51eb2c0ee5b02df6a82446401d307456
Ethan <ethanlegend7@gmail.com>
2026-03-23 14:00:05 +1100

    adjust hero text

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -8,14 +8,15 @@ const Hero = () => {
           alt=""
           width={8000}
           height={8000}
+          className="select-none"
         />
-        <h1 className="absolute top-1/4 left-1/4 -translate-y-1/4 -translate-x-1/4 ">
-          <span className="italic text-warmwhite text-7xl font-cormorant ">
-            Styled
+        <h1 className="absolute top-1/3 left-1/6 lg:left-1/4 -translate-y-1/3 -translate-x-1/6 lg:-translate-x-1/4 ">
+          <span className=" text-white text-base md:text-4xl  uppercase font-light font-montserrat  ">
+            Styled to
           </span>{" "}
           <br />
-          <span className="uppercase text-white text-[5.625rem] font-montserrat font-extrabold ">
-            to sell
+          <span className="italic uppercase text-warmwhite text-[42px] md:text-8xl font-cormorant font-extrabold ">
+            sell
           </span>
         </h1>
       </div>
